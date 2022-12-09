9ce44bdfc006a7abb2ad00d8737146c8548e9248
KarnoL25 <1194823379@qq.com>
2022-12-09 21:41:22 +0800

    完成numberPad虚拟数字键盘

--- src/components/homeComponents/Write.vue
+++ src/components/homeComponents/Write.vue
@@ -6,9 +6,33 @@
     </label>
     <label class="write">
       <span>数目</span>
-      <input type="number" placeholder="在此输入数字" oninput="if(value.length>16)value=value.slice(0,16)" v-model="numberValue">
+      <input 
+      type="text" 
+      maxlength="2"
+      placeholder="在此输入数字" 
+      oninput="if(value.length>16)value=value.slice(0,16)" 
+      v-model="numberValue"
+      @focus="focus"
+      >
       <!-- 限制长度 -->
     </label>
+    <div v-show="visible">
+      <p class="back" @click="blur">收起键盘</p>
+      <div class="numberPad">
+        <span @click=keyEvent(1)>1</span>
+        <span @click=keyEvent(2)>2</span>
+        <span @click=keyEvent(3)>3</span>
+        <span @click=keyEvent(4)>4</span>
+        <span @click=keyEvent(5)>5</span>
+        <span @click=keyEvent(6)>6</span>
+        <span @click=keyEvent(7)>7</span>
+        <span @click=keyEvent(8)>8</span>
+        <span @click=keyEvent(9)>9</span>
+        <span @click=keyEvent(0)>0</span>
+        <span @click=dot>.</span>
+        <span @click="remove">删除</span>
+      </div>
+    </div>
   </div>
 </template>
  
@@ -17,7 +41,35 @@ export default {
   data() {
     return {
       writeValue:'',
-      numberValue:''
+      numberValue:'',
+      visible:false
+    }
+  },
+  methods:{
+    focus(){
+      this.visible = true
+    },
+    blur(){
+      this.visible = false
+    },
+    keyEvent(key){
+      if(this.numberValue === '0'){
+        if(key === '0'){return}
+      }else if(this.numberValue.length>16){return}
+      else{this.numberValue += key}
+    },
+    dot(){
+      if(this.numberValue === ''){
+        return 
+      }else if(this.numberValue.includes('.')){
+        return
+      }else{
+        this.numberValue += '.'
+      }
+    },
+    remove(){
+      const number = this.numberValue.slice(0,-1)
+      this.numberValue = number
     }
   },
   watch:{
@@ -55,6 +107,33 @@ export default {
     &:focus{
     outline:1px solid #fab949;
     }
+  }
+}
+.back{
+  position: absolute;
+  bottom: 360px;
+  height: 40px;
+  width: 100%;
+  text-align: center;
+  background: rgb(223, 237, 255);
+  font-family: "Gill Sans", sans-serif;
+  padding: 10px 0 0 0;
+}
+.numberPad{
+  position: absolute;
+  bottom: 60px;
+  height: 300px;
+  width: 100%;
+  background: white;
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  span{
+    font-size: 1.5em;
+    font-family: "Gill Sans", sans-serif;
+    width: 33%;
+    padding: 30px 0 0 0;
+    border: 1px solid #ccc;
   }
 }
 </style>
